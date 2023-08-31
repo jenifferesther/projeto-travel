d93ae229d217f665265ca6c495ffeaf1787fb0ed
jenifferesther <jes21@aluno.ifal.edu.br>
2023-08-31 16:33:59 -0300

    adicionou logo , cores e criando um arquivo.

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,34 @@
+import { getDisplayName } from 'next/dist/shared/lib/utils'
 import './globals.css'
+import styles from './styles.module.css'
+// /**/import {Quicksand} from 'next/font/google'
 
 export const metadata = {
   title: ' Travel Landing Page ',
 }
 
+// const inter =Quicksand({
+//   subsets:['latin']
+//   display:''
+// })
+
+function Logo(){
+  return(
+    <p className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+  );
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body >
+        <header>
+          <Logo/>
+        </header>
+        <main>
+        {children}
+        </main>
+        </body>
     </html>
   )
 }
